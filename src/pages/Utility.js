eacda9c53dc074cf1d0de7852bf24bc57cc5819f
nattacha <natchaaer@gmail.com>
2021-05-21 20:16:21 +0700

    [UPDATE] utility.js

---
+++ src/pages/Utility.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Utility = () => {
+  return (
+    <div>
+      <h1 class="text-dark text-center">Utilities Calculation</h1>
+
+     <div class="container mt-3">
+     <h5>Month : </h5>
+     </div>
+   </div>
+  );
+};
+
+export default Utility;
