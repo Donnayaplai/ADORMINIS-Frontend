fdcd21cf331701635c6d0826182ad8c568397378
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-06-14 20:24:38 +0700

    [EDIT] page utility

--- src/pages/Utility.js
+++ src/pages/Utility.js
@@ -3,11 +3,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Utility = () => {
   return (
-    <div>
-      <h1 class="text-dark text-center">Utilities Calculation</h1>
+    <div className="Room mb-5">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-xl-9 col-md-8 mx-auto ">
+            <h1 className="text-dark text-center">Utilities Calculation</h1>
+          </div>
+        </div>
+        <div class="col-xl-10 col-lg-8 col-md-6">
+          <input
+            type="text"
+            class="form-control"
+            id="search"
+            placeholder="Search here"
+          />
+        </div>
+      </div>
 
-      <div class="container mt-3">
-        <h5>Month : </h5>
+      <div>
+        <div className="container">
+          <table>
+            <thead>
+              <tr>
+                <th>Room</th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>Water</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Electricity</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
