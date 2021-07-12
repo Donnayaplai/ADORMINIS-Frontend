177ffd3360386504a1d5a6f5ef5734ecc4bc2bea
Donnaya <donnayawang@gmail.com>
2021-07-12 19:38:00 +0700

    [EDIT] Classname Room.js

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import "../styles/App.css";
-function Room() {
+function Room(props) {
   const [data, SetData] = useState([]);
   let rowstyle = {
     backgroundColor: "#EAE7E2",
@@ -43,18 +43,18 @@ function Room() {
                 placeholder="Type to search..."
               ></input>
             </div>
-            <div class="col-auto">
-              <button class="btn btn-lg btn-success" type="submit">
+            <div className="col-auto">
+              <button className="btn btn-lg btn-success" type="submit">
                 Search
               </button>
             </div>
           </form>
         </div>
       </div>
-      <div class="container overflow-hidden">
-        <div class="row gx-3">
-          <div class="col-7">
-            <div class="border">
+      <div className="container overflow-hidden">
+        <div className="row gx-3">
+          <div className="col-7">
+            <div className="border">
               <div className="table-responsive rounded">
                 <table
                   className="table table-hover align: middle table-borderless"
@@ -76,7 +76,7 @@ function Room() {
                         <td>
                           <button
                             type="button"
-                            class="btn btn-light"
+                            className="btn btn-light"
                             data-bs-toggle="modal"
                             data-bs-target="#exampleInfo"
                             data-bs-whatever="@mdo"
@@ -84,45 +84,44 @@ function Room() {
                             Info
                           </button>
                           <div
-                            class="modal fade"
+                            className="modal fade"
                             id="exampleInfo"
-                            tabindex="-1"
+                            tabIndex="-1"
                             aria-labelledby="exampleModalInfo"
                             aria-hidden="true"
                           >
-                            <div class="modal-dialog">
-                              <div class="modal-content">
+                            <div className="modal-dialog">
+                              <div className="modal-content">
                                 <div
-                                  class="modal-header"
+                                  className="modal-header"
                                   style={{ backgroundColor: "#C7E5F0" }}
                                 >
                                   <h5
-                                    class="modal-title"
+                                    className="modal-title"
                                     id="exampleModalInfo"
                                   >
                                     Resident Information
                                   </h5>
                                   <button
                                     type="button"
-                                    class="btn-close"
+                                    className="btn-close"
                                     data-bs-dismiss="modal"
                                     aria-label="Close"
                                   ></button>
                                 </div>
-                                <div class="modal-body">
-                                  Information
-                                </div>
-                                <div class="modal-footer">
+                                <div className="modal-body">Information</div>
+                                <div className="modal-footer">
                                   <button
                                     type="button"
-                                    class="btn btn-secondary"
+                                    className="btn btn-secondary"
                                     data-bs-dismiss="modal"
                                   >
                                     Close
                                   </button>
                                   <button
                                     type="button"
-                                    class="btn btn-primary">
+                                    className="btn btn-primary"
+                                  >
                                     Ok
                                   </button>
                                 </div>
@@ -130,10 +129,10 @@ function Room() {
                             </div>
                           </div>
                         </td>
-                        <td>
+                        <td key={i.id}>
                           <button
                             type="button"
-                            class="btn btn-light"
+                            className="btn btn-light"
                             data-bs-toggle="modal"
                             data-bs-target="#exampleModal"
                             data-bs-whatever="@mdo"
@@ -141,42 +140,42 @@ function Room() {
                             Add
                           </button>
                           <div
-                            class="modal fade"
+                            className="modal fade"
                             id="exampleModal"
-                            tabindex="-1"
+                            tabIndex="-1"
                             aria-labelledby="exampleModalLabel"
                             aria-hidden="true"
                           >
-                            <div class="modal-dialog">
-                              <div class="modal-content">
+                            <div className="modal-dialog">
+                              <div className="modal-content">
                                 <div
-                                  class="modal-header"
+                                  className="modal-header"
                                   style={{ backgroundColor: "#C7E5F0" }}
                                 >
                                   <h5
-                                    class="modal-title"
+                                    className="modal-title"
                                     id="exampleModalLabel"
                                   >
                                     Resident Code
                                   </h5>
                                   <button
                                     type="button"
-                                    class="btn-close"
+                                    className="btn-close"
                                     data-bs-dismiss="modal"
                                     aria-label="Close"
                                   ></button>
                                 </div>
-                                <div class="modal-body">
+                                <div className="modal-body">
                                   <form>
-                                    <div class="mb-3">
+                                    <div className="mb-3">
                                       <label
-                                        for="message-text"
-                                        class="col-form-label"
+                                        htmlFor="message-text"
+                                        className="col-form-label"
                                       >
                                         Resident Code:
                                       </label>
                                       <textarea
-                                        class="form-control"
+                                        className="form-control"
                                         id="message-text"
                                         placeholder="Please enter your resident code here..."
                                       ></textarea>
@@ -184,52 +183,63 @@ function Room() {
                                   </form>
                                 </div>
                                 <p>No resident code?</p>
-                                <div class="modal-footer">
+                                <div className="modal-footer">
                                   <button
                                     type="button"
-                                    class="btn btn-secondary"
+                                    className="btn btn-secondary"
                                     data-bs-dismiss="modal"
                                   >
                                     Close
                                   </button>
                                   <button
                                     type="button"
-                                    class="btn btn-primary"
+                                    className="btn btn-primary"
                                     data-bs-toggle="modal"
                                     data-bs-target="#addinfomodal"
                                   >
                                     Confirm
                                   </button>
-                                  <div class="modal fade" 
-                                  id="addinfomodal" 
-                                  tabindex="-1" 
-                                  aria-labelledby="addinfoModalLabel" 
-                                  aria-hidden="true"
+                                  <div
+                                    className="modal fade"
+                                    id="addinfomodal"
+                                    tabIndex="-1"
+                                    aria-labelledby="addinfoModalLabel"
+                                    aria-hidden="true"
                                   >
-                                    <div class="modal-dialog">
-                                      <div class="modal-content">
-                                        <div class="modal-header">
-                                          <h5 
-                                          class="modal-title"
-                                           id="addinfoModalLabel">Resident Information</h5>
-                                          <button 
-                                          type="button" 
-                                          class="btn-close" 
-                                          data-bs-dismiss="modal" 
-                                          aria-label="Close">
-
+                                    <div className="modal-dialog">
+                                      <div className="modal-content">
+                                        <div className="modal-header">
+                                          <h5
+                                            className="modal-title"
+                                            id="addinfoModalLabel"
+                                          >
+                                            Resident Information
+                                          </h5>
+                                          <button
+                                            type="button"
+                                            className="btn-close"
+                                            data-bs-dismiss="modal"
+                                            aria-label="Close"
+                                          ></button>
+                                        </div>
+                                        <div className="modal-body">
+                                          Personal Information Additional
+                                          Information
+                                        </div>
+                                        <div className="modal-footer">
+                                          <button
+                                            type="button"
+                                            className="btn btn-secondary"
+                                            data-bs-dismiss="modal"
+                                          >
+                                            Cancle
                                           </button>
-                                        </div>
-                                        <div class="modal-body">
-                                          Personal Information
-                                          Additional Information
-                                        </div>
-                                        <div 
-                                        class="modal-footer">
-                                          <button type="button" 
-                                          class="btn btn-secondary" 
-                                          data-bs-dismiss="modal">Cancle</button>
-                                          <button type="button" class="btn btn-primary">Save</button>
+                                          <button
+                                            type="button"
+                                            className="btn btn-primary"
+                                          >
+                                            Save
+                                          </button>
                                         </div>
                                       </div>
                                     </div>
@@ -246,18 +256,16 @@ function Room() {
               </div>
             </div>
           </div>
-          <div class="col-5">
+          <div className="col-5">
             <div
-              class="p-3 border"
+              className="p-3 border"
               style={{ backgroundColor: "#C7E5F0", maxHeight: "400px" }}
             ></div>
           </div>
         </div>
       </div>
     </div>
-
   );
-
 }
 export default Room;
 
@@ -309,10 +317,10 @@ export default Room;
 //             <h1 className="text-dark text-center">Room</h1>
 //           </div>
 //         </div>
-//         <div class="col-xl-10 col-lg-8 col-md-6">
+//         <div className="col-xl-10 col-lg-8 col-md-6">
 //           <input
 //             type="text"
-//             class="form-control"
+//             className="form-control"
 //             id="search"
 //             placeholder="Search here"
 //           />
@@ -364,24 +372,24 @@ export default Room;
 //   );
 // }
 // export default Room;
-/* <div class="container mt-4 text-center">
-        <div class="row">
-          <div class="col-xl-10 col-lg-8 col-md-6">
+/* <div className="container mt-4 text-center">
+        <div className="row">
+          <div className="col-xl-10 col-lg-8 col-md-6">
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="search"
               placeholder="Search here"
             />
           </div>
         </div>
       </div>
-      <div class="container mt-3">
+      <div className="container mt-3">
         <h5>Building : </h5>
         <h5>Floor : </h5>
       </div>
       <div className="container">
-        <table class="table table-hover">
+        <table className="table table-hover">
           <thead>
             <tr>
               <th scope="col">Room</th>
@@ -395,12 +403,12 @@ export default Room;
               <th scope="row">101</th>
               <td>not available</td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Info
                 </button>
               </td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Add
                 </button>
               </td>
@@ -409,12 +417,12 @@ export default Room;
               <th scope="row">102</th>
               <td>not available</td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Info
                 </button>
               </td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Add
                 </button>
               </td>
@@ -423,12 +431,12 @@ export default Room;
               <th scope="row">103</th>
               <td>available</td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Info
                 </button>
               </td>
               <td>
-                <button type="button" class="btn btn-light">
+                <button type="button" className="btn btn-light">
                   Add
                 </button>
               </td>
@@ -436,7 +444,7 @@ export default Room;
           </tbody>
         </table>
       </div>
-      <div class="container mt-3">
+      <div className="container mt-3">
         <h5>Amount of Room Available : </h5>
         <h5>Amount of Room Not Available : </h5>
 </div> */
