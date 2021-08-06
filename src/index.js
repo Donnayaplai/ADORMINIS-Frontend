d368aa0651db6dcca4700a9a6f9dd987b349f413
Donnaya <donnayawang@gmail.com>
2021-08-06 09:20:15 +0700

    [EDIT] Index files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import "./styles/index.css";
+import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
