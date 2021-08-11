720cab76e7309a56e9800bf242e3e2c37fcd1e27
Donnaya <donnayawang@gmail.com>
2021-08-11 08:28:00 +0700

    [EDIT] Invoice

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -1,24 +1,75 @@
 import React from "react";
 
 function Invoice() {
+  let rowstyle = {
+    backgroundColor: "#EAE7E2",
+    border: "none",
+    textAlign: "center",
+  };
+  let thead = {
+    backgroundColor: "#C7E5F0",
+    textAlign: "center",
+    color: "black",
+    fontWeight: "bold",
+    fontSize: "18px",
+    height: "30px",
+  };
+  let button = {
+    backgroundColor: "#C7E5F0",
+    textAlign: "center",
+    color: "black",
+    fontSize: "18px",
+    height: "50px",
+    maxHeight: "50px",
+    width: "100%",
+    maxWidth: "300px",
+  };
   return (
-    <div>
-      <h1 className="text-dark text-center">Invoice</h1>
-      <div class="col-xl-10 col-lg-8 col-md-6">
+    <div className="container">
+      <h1 className="text-center">สถานะการชำระเงิน</h1>
+      {/* <div className="" style={{ backgroundColor: "#C7E5F0" }}> */}
+      <div className="table-responsive mt-3">
+        <table className="table table-hover align-middle table-borderless">
+          <thead style={thead}>
+            <tr>
+              <th scope="col">เดือน</th>
+              <th scope="col">สถานะ</th>
+              <th scope="col">วันที่ชำระเงิน</th>
+            </tr>
+          </thead>
+          <tbody style={rowstyle}>
+            <tr>
+              <th scope="row">04/2021</th>
+              <td>ยังไม่ได้ชำระเงิน</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <th scope="row">03/2021</th>
+              <td>ชำระเงินแล้ว</td>
+              <td>30/03/2021</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button type="button" className="btn mt-3" style={button}>
+        Make a payment
+      </button>
+      {/* </div> */}
+      {/* <h1 className="text-dark text-center">Invoice</h1>
+      <div className="col-xl-10 col-lg-8 col-md-6">
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="search"
               placeholder="Search here"/>
           </div>
           <div className="row">
-          <div class="container mt-3">
+          <div className="container mt-3">
         <h5>Year : </h5>
         <h5>Month : </h5>
       </div>
-</div>
+</div> */}
     </div>
-     
   );
 }
 export default Invoice;
