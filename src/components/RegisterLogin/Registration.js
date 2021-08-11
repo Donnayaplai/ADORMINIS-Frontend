870a119fd0a0b0df582e816b97c487c849555e3c
Donnaya <donnayawang@gmail.com>
2021-08-11 10:33:37 +0700

    [EDIT] Registration css path

--- src/components/RegisterLogin/Registration.js
+++ src/components/RegisterLogin/Registration.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import "../components/RegisterLogin/Register.css";
+import "../RegisterLogin/Register.css";
 
 function Registration() {
   const buttonsubmit = {
