1266d35a12253c8a5efc88399f36f24b09da481b
Donnaya <donnayawang@gmail.com>
2021-08-18 09:33:37 +0700

    [EDIT] Search and filter room

--- src/components/Admin/Room/Room.js
+++ src/components/Admin/Room/Room.js
@@ -15,28 +15,15 @@ const Room = () => {
   return (
     <div className="container">
       <h1 className="text-center">สถานะห้องพัก</h1>
-      <div className="row justify-content-center mt-3 mb-3">
-        <div className="col-12 col-md-10 col-lg-8">
-          <div className="input-group">
-            <input
-              type="search"
-              className="form-control rounded"
-              placeholder="พิมพ์เพื่อค้นหา"
-              aria-label="Search"
-              aria-describedby="search-addon"
-            />
-            <button type="button" className="btn btn-outline-primary">
-              search
-            </button>
-          </div>
-        </div>
+      <div className="row justify-content-center mt-3">
+        <div className="col-12 col-md-10 col-lg-8"></div>
       </div>
       <div className="container overflow-hidden">
         <div className="row gx-4">
           <div className="col-7">
             <AllRoom />
           </div>
-          <div className="col-5">
+          <div className="col-5 mt-5">
             <ShowRoomAmount />
           </div>
         </div>
